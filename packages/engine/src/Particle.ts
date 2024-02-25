ab3620badce6c405bb9adb37c02207792f318783
Nikolay Kuznetsov <kolaynus@gmail.com>
2024-02-26 01:21:40 +0300

    wip: создание универсальной частицы

--- packages/engine/src/Particle.ts
+++ packages/engine/src/Particle.ts
@@ -230,3 +230,221 @@ export const Species = {
 
 export type SpeciesName = keyof typeof Species;
 export type SpeciesValue = typeof Species[SpeciesName]
+
+
+type TParticle = {
+    type: 'solid' | 'liquid' | 'gas' | 'plasma'
+    clock: number
+    baseColor: string
+}
+
+type TPhisicalParticle = {
+    state: 'solid' | 'liquid' | 'gas' | 'plasma'
+    density: number
+    temperature: number
+}
+
+type TSolidParticle = {
+    /** сыпучий */
+    loosy: boolean
+    density: number
+
+    /** Температура перехода в жидкое состояние | плавление */
+    meltingTemperature: number
+    /** Температура перехода в газообразное состояние | возгонка */
+    sublimationTemperature: number
+}
+
+// meltingTemperature === freezingTemperature          | solidLiquidTransitionTemperature
+// sublimationTemperature === depositionTemperature    | solidGasTransitionTemperature
+// vaporizationTemperature === condensationTemperature | liquidGasTransitionTemperature
+// ionizationTemperature === recombinationTemperature  | gasPlasmaTransitionTemperature
+
+// сгорание - переход из плазмы в ничто, количество тиков
+// плазма - частица, возницающая из газа, который порождает высоконагретая материя
+// температура распространяется от частицы к частице с потерей (качество распространения зависит от теплопередачи)
+// нагретая твёрдая частица, в зависимости от температуры либо становится жидкой, либо газообразной
+// в следующий тик, если был газ, он либо остаётся таким, либо становится плазмой?
+// либо при горении он испускает плазму, имеющую более высокую температуру
+
+type TLiquidParticle = {
+    density: number
+    /** вязкость */
+    viscosity: number
+
+    /** Температура перехода в газообразное состояние | испарение */
+    vaporizationTemperature: number
+    /** Температура перехода в твердое состояние | кристаллизация */
+    freezingTemperature: number
+}
+
+type TGasParticle = {
+    density: number
+    viscosity: number
+
+    /** Температура перехода в жидкое состояние | конденсация */
+    condensationTemperature: number
+    /** Температура перехода в твердое состояние | десублимация */
+    depositionTemperature: number
+    /** Температура перехода в плазменное состояние | ионизация? */
+    ionizationTemperature: number
+}
+
+type TPlasmaParticle = {
+    temperature: number
+    /** Температура перехода в газообразное состояние | рекомбинация */
+    recombinationTemperature: number
+}
+
+// фазовые переходы
+// реакции
+
+class SolidParticle {
+    static type = 'solid'
+
+    update (cell: SolidParticle, api: Api) {
+
+    }
+}
+class LiquidParticle {
+    static type = 'liquid'
+
+    update (cell: LiquidParticle, api: Api) {
+
+    }
+}
+class GasParticle {
+    static type = 'gas'
+
+    update (cell: GasParticle, api: Api) {
+
+    }
+}
+class PlasmaParticle {
+    static type = 'plasma'
+
+    update (cell: PlasmaParticle, api: Api) {
+
+    }
+}
+
+
+function update (cell: any, api: Api) {
+    updateState(cell)
+}
+
+function updateState (cell: any) {
+    cell.prevState = cell.state
+
+    // upgrade
+
+    if (cell.state === 'solid' && cell.temperature >= cell.solidLiquidTransitionTemperature) {
+        cell.state = 'liquid'
+    }
+
+    if (cell.state === 'solid' && cell.temperature >= cell.solidGasTransitionTemperature) {
+        cell.state = 'gas'
+    }
+
+    if (cell.state === 'liquid' && cell.temperature >= cell.liquidGasTransitionTemperature) {
+        cell.state = 'gas'
+    }
+
+    if (cell.state === 'gas' && cell.temperature >= cell.gasPlasmaTransitionTemperature) {
+        cell.state = 'plasma'
+    }
+
+    // downgrade
+
+    if (cell.state === 'plasma' && cell.temperature < cell.gasPlasmaTransitionTemperature) {
+        cell.state = 'gas'
+    }
+
+    if (cell.state === 'gas' && cell.temperature < cell.liquidGasTransitionTemperature) {
+        cell.state = 'liquid'
+    }
+
+    if (cell.state === 'gas' && cell.temperature < cell.solidGasTransitionTemperature) {
+        cell.state = 'solid'
+    }
+
+    if (cell.state === 'liquid' && cell.temperature < cell.solidLiquidTransitionTemperature) {
+        cell.state = 'solid'
+    }
+}
+
+function move (cell: any, api: Api) {
+    if (cell.state === 'solid') moveSolid (cell, api)
+    if (cell.state === 'liquid') moveLiquid (cell, api)
+    if (cell.state === 'gas') moveGas (cell, api)
+    if (cell.state === 'plasma') movePlasma (cell, api)
+}
+
+function moveSolid (cell: any, api: Api) {
+    if (!cell.loosy) return
+
+    const below = api.get(0, 1) as any
+
+    if (below.state === null) {
+        api.set(0, 0, below)
+        api.set(0, 1, cell)
+        return
+    }
+
+    if (below.state === 'liquid') {
+
+        return
+    }
+
+    if (below.state === 'gas') {
+
+        return
+    }
+
+    if (below.state === 'plasma') {
+
+        return
+    }
+
+    const isSliding = random(0, 1000) > cell.density // isShifting
+
+    if (isSliding) {
+        const dx = api.randomDir2()
+        const belowSide = api.get(dx, 1) as any
+
+        if (api.get(dx, 1).state === null) {
+            api.set(0, 0, api.get(dx, 1))
+            api.set(dx, 1, cell)
+        } else if (api.get(-dx, 1).state === null) {
+            api.set(0, 0, api.get(-dx, 1))
+            api.set(-dx, 1, cell)
+        }
+    }
+
+    api.set(0, 0, cell)
+
+
+
+    if (belowSide.state === null) {
+        api.set(0, 0, belowSide)
+        api.set(dx, 1, this)
+    } else if (below.species === Species.Water) {
+        api.set(0, 0, below)
+        api.set(0, 1, this)
+    } else {
+        api.set(0, 0, this)
+    }
+
+}
+
+function moveLiquid (cell: any, api: Api) {
+
+}
+
+function moveGas (cell: any, api: Api) {
+
+}
+
+function movePlasma (cell: any, api: Api) {
+
+}
